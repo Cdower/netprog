90e16a987b13d46cab0ed79f51249f94de19168f
Chris Dower <devwowow@gmail.com>
2016-04-29 07:26:04 -0400

    working 100%, except it does not exit on completion, stalls on BIO_read(bio, buf, 1024)

--- hw5/HTTP(S)client.cpp
+++ hw5/HTTP(S)client.cpp
@@ -26,19 +26,23 @@ struct url_p{
 };
 
 int main(int argc, char * argv[]){
+  SSL_library_init();
+  SSL_load_error_strings();
+  OpenSSL_add_all_algorithms();
+  BIO *bio, *out;
+  SSL_CTX *ctx;
+  SSL *ssl;
   url_p parsed;
-  char queryHost[256];
-  struct addrinfo *first, hints, *i;
-  int sockfd, ret;
+  char hostPort[512], request[1024], buf[1024];
+  int len;
+  size_t hostSplit;
+  string host, port, token, path="", service;
+  string sURL(argv[1]);
   if(argc < 2){
     perror("Requires input url to parse.");
     exit(EXIT_FAILURE);
   }
   //Start parseURL
-  size_t hostSplit;
-  string host, port, token, path, service;
-  string sURL(argv[1]);
-
   service = sURL.substr(0,sURL.find("://"));
   hostSplit = sURL.find("www");
   hostSplit!= string::npos ? (token=sURL.substr(hostSplit)) : (token = token);
@@ -48,76 +52,80 @@ int main(int argc, char * argv[]){
   if(hostSplit != string::npos){
     host = token.substr(0,hostSplit);
     port = token.substr(hostSplit+1);
-    (hostSplit=port.find('/')) != string::npos ? (port = port.substr(0,hostSplit) ) : (port = port) ;
+    (hostSplit=port.find('/')) != string::npos ? (port = port.substr(0,hostSplit) ) : (port = port);
+    if(port.compare("443")!=0){
+      parsed.secure = true;
+    }else { parsed.secure = false; }
   }else{
     hostSplit = token.find('/');
     hostSplit!=string::npos ? host = token.substr(0,hostSplit) : host = token;
     if(service.compare("https")==0){
       port="443";
       parsed.secure = true;
-      SSL_library_init();
-      SSL_load_error_strings();
     }else{
       port="80";
       parsed.secure = false;
     }
   }
-  (hostSplit=token.find('/')) != string::npos ? path = token.substr(hostSplit) : path="";
-
+  (hostSplit=token.find('/')) != string::npos ? path = token.substr(hostSplit) : path=path;
   parsed.host = host.c_str();
   parsed.path = path.c_str();
   parsed.port = port.c_str();
-  strcpy(queryHost, parsed.host);
+  sprintf(request, "GET %s HTTP/1.1\nHost: %s:%s\nUser-Agent: dowerc-netprog-hw3/1.0\n\n", parsed.path, parsed.host, parsed.port);
   //End parseURL
-  //Begin connect to host
-  int lenHost = strlen(parsed.host), lenPort = strlen(parsed.port);
-  char *host_c  = new char[lenHost+1](), *port_c = new char[lenPort+1]();
-  strncpy(host_c, parsed.host, lenHost);
-  strncpy(port_c, parsed.port, lenPort);
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
+  ///Intialize OpenSSL
 
-  ///Initialize & Connect TCP Socket
-  if(ret = getaddrinfo(host_c, port_c, &hints, &first) != 0){
-      fprintf(stderr, "getaddrinfo() failed: %s\n", gai_strerror(ret));
-      exit(1);
-  }
-  //cout << "getaddr success" << cout;
-  for(i=first; i!= NULL; i=i->ai_next){
-    if((sockfd = socket(i->ai_family, i->ai_socktype, i->ai_protocol)) == -1 ){
-      perror("socket");
-      continue;
+  if(parsed.secure){ //do SSL
+    strcpy(hostPort, (host + ":" + port).c_str());
+    ctx = SSL_CTX_new(SSLv23_client_method());
+    bio = BIO_new_ssl_connect(ctx);
+    BIO_get_ssl(bio, &ssl);
+    if(!ssl) { fprintf(stderr, "Can't locate SSL pointer\n"); }
+    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
+    BIO_set_conn_hostname(bio, hostPort);
+    out = BIO_new_fp(stdout, BIO_CLOSE);
+    if(BIO_do_connect(bio) <= 0){
+      fprintf(stderr, "Error connecting to server\n");
+       ERR_print_errors_fp(stderr);
     }
-    bind(sockfd, (struct sockaddr *) &i, sizeof(i));
-    if( connect( sockfd, (i->ai_addr), i->ai_addrlen) == -1){
-      cout << "Connect Failure" << endl;
-      close(sockfd);
-      perror("connect");
-      continue;
+    if(BIO_do_handshake(bio) <= 0){
+      fprintf(stderr, "Error establishing SSL connection\n");
+      ERR_print_errors_fp(stderr);
     }
-    break;
+    cout << request << endl;
+    BIO_puts(bio, request);
+    for(;;){
+      len = BIO_read(bio, buf, 1024);
+      if(len>0){
+        BIO_write(out, buf, len);
+      }else{
+        cout << "\n\nLEN:: " << len << endl;
+        break;
+      }
+    }
+    BIO_free_all(bio);
+    BIO_free(out);
+  }else{ // do regular connect
+    strcpy(hostPort, (host + ":" + port).c_str());
+    bio = BIO_new_connect( hostPort );
+    out = BIO_new_fp(stdout, BIO_CLOSE);
+    if(BIO_do_connect(bio) <= 0){
+      fprintf(stderr, "Error connecting to server\n");
+      ERR_print_errors_fp(stderr);
+    }
+    cout << request << endl;
+    BIO_puts(bio, request);
+    for(;;){
+      len = BIO_read(bio, buf, 1024);
+      if(len>0){
+        BIO_write(out, buf, len);
+      }else{
+        cout << "\n\nLEN:: " << len << endl;
+        break;
+      }
+    }
+    BIO_free_all(bio);
+    BIO_free(out);
   }
-  //sendRequest(queryPath, queryHost, queryPort, sockfd);
-  ///Begin sendRequest to build and send GET request
-  char request[1024];
-  sprintf(request, "GET %s HTTP/1.1\r\nHost: %s:%s\r\nUser-Agent: dowerc-netprog-hw3/1.0\r\n\r\n", parsed.path, queryHost, parsed.port);
-  if(send(sockfd, request, sizeof(request), 0) < 0){
-    cout << "Not written: " << request << endl;
-  }else{cout << "Sent:\n" << request << endl;}
-  ///End sendRequest
-  ///Begin listenResponse//listenResponse(sockfd);
-  char buf[1024];
-  int len, total = 2147483647; //INTMAX
-
-  cout << "Recieved: " << endl;
-  while( (len = recv(sockfd, buf, sizeof(buf)-1, 0)) > 0 && total > 0){
-    buf[len] = '\0';
-    cout << buf << endl;
-    memset(buf, 0, len);
-  }
-  fclose(stdout);
-  close(sockfd);
-
   return EXIT_SUCCESS;
 }
